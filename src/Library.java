ba63a896a78f7acf4decc8b4b043c89a789ef12d
geaney91 <geaneymichael@yahoo.ie>
2015-10-28 14:54:56 +0000

    Finished login, decorator and strategy and Library and Tracking File and stuff

--- src/Library.java
+++ src/Library.java
@@ -2,30 +2,46 @@ import java.io.*;
 import java.util.*;
 public class Library
 {
+
     public Library()
     {
     }
 
     Scanner in = new Scanner(System.in);
-    public void readFile() throws IOException
-    {
-        BufferedReader br = new BufferedReader(new FileReader("Login.txt"));
-        String list;
-        String contents="";
-        while((list = br.readLine()) != null){
-            contents+= list;
-            //String contents = br.readLine();
-            // return contents;
-        }
-       // return contents;
-    }
 
-    public void writeFile(String ID,String Spass,String name,String password) throws IOException
+    public void writeFile(String user, String pass) throws IOException
     {
-        PrintWriter out = new PrintWriter(new FileWriter("Login.txt",true));
-        out.println("Student,"+ ID + ","+Spass+",");
+        int range = (10000);
+        int id = (int)(Math.random() * range);
+        PrintWriter out = new PrintWriter( new BufferedWriter(new FileWriter("./src/Users.txt",true)));
+        out.println(user + "," + pass + "," + id);
         out.close();
     }
 
+    public void writeFile(String user, int id, Date deliveryDate) throws IOException
+    {
+        Date sent = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(sent);
+        int range = (259);
+        int letterId = (int)(Math.random() * range);
+        PrintWriter out = new PrintWriter(new FileWriter("./src/PostTracking.txt",true));
+        out.println(user + "," + id + IntToLetter(letterId) + "," + sent + "," + deliveryDate);
+        out.close();
+    }
+
+    public static String IntToLetter(int Int)
+    {
+        if (Int<27){
+            return Character.toString((char)(Int+96));
+        } else {
+            if (Int%26==0) {
+                return IntToLetter((Int/26)-1)+IntToLetter((Int%26)+1);
+            } else {
+                return IntToLetter(Int/26)+IntToLetter(Int%26);
+            }
+        }
+
+        }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,57 +3,71 @@ import java.io.*;
 public class Main {
     public static void main(String[]args) throws IOException
     {
-
-        PostFactory postFactory = new PostFactory();
         Scanner in = new Scanner(System.in);
-
-
-        if (Login() == true) {
+        boolean check = Login();
+        if (check == true)
+        {
             System.out.println("Logged In");
+            Details();
 
-            System.out.print("Enter type of Post:(Letter, Parcel, Package) ");
-            String post = in.nextLine();
-
-
-            Post post1 = postFactory.getPost(post);
-
-
-            System.out.print("Enter destination to deliver to: (Ireland, UK, Europe) ");
-            String c = in.nextLine();
-
-            Post countryPost = new CountryPostDecorator(post1, c);
-
-
-            System.out.print("Enter weight in kg: ");
-            double w = in.nextDouble();
-            in.nextLine();
-
-            Post weightedPost = new WeightPostDecorator(countryPost, w);
-            //weightedPost.create();
-
-            System.out.print("Enter delivery type: (Standard, Express, Super)");
-            String delivery  = in.nextLine();
-
-            Post deliveryPost = new DeliveryPostDecorator(weightedPost, delivery);
-
-            System.out.println("Postage Cost: " + deliveryPost.GetPrice());
-
-            Context context = new Context(new DeliveryDate());
-            System.out.println(context.executeStrategy(delivery, c));
-
-            Subject subject = new Subject();
-            new ObserverPost(subject);
-
-            //subject.setState(true);
-
-
-            // vb.compareTo(array[x])
-
-            //post1.create();
-            // System.out.print("You have selected " + post + " of " + weightedPost.getWeight() + "kg");
         }
         else
-            System.out.println("No Log in today");
+        {
+            while (check == false)
+            {
+                System.out.println("User not found. Register or Log: ");
+                String start = in.nextLine();
+                if(start.equalsIgnoreCase("log"))
+                check = Login();
+                else if(start.equalsIgnoreCase("register"))
+                {
+                    Register();
+                    Details();
+                }
+                else
+                    System.exit(0);
+
+            }
+            Details();
+
+        }
+    }
+
+    public static void Details() throws IOException {
+        PostFactory postFactory = new PostFactory();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter type of Post:(Letter, Parcel, Package) ");
+        String post = in.nextLine();
+        Post post1 = postFactory.getPost(post);
+
+
+        System.out.print("Enter destination to deliver to: (Ireland, UK, Europe) ");
+        String c = in.nextLine();
+        Post countryPost = new CountryPostDecorator(post1, c);
+
+
+        System.out.print("Enter weight in kg: ");
+        double w = in.nextDouble();
+        in.nextLine();
+        Post weightedPost = new WeightPostDecorator(countryPost, w);
+
+        System.out.print("Enter delivery type: (Standard, Express, Super)");
+        String delivery  = in.nextLine();
+        Post deliveryPost = new DeliveryPostDecorator(weightedPost, delivery);
+
+        System.out.println("Postage Cost: " + deliveryPost.GetPrice());
+
+        Context context = new Context(new DeliveryDate());
+        System.out.println(context.executeStrategy(delivery, c));
+        Date date = context.executeStrategy(delivery, c);
+
+        Subject subject = new Subject();
+        new ObserverPost(subject);
+
+        Library library = new Library();
+        int id = 69;
+        String name = "JJ";
+        library.writeFile(name, id, date);
 
     }
 
@@ -78,11 +92,24 @@ public class Main {
                 break;
             }
             else
+            {
                 check = false;
+            }
         }
 
         return check;
 
+    }
+    public static void Register() throws IOException
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter username:");
+        String name = in.nextLine();
+        System.out.print("Enter password:");
+        String pass = in.nextLine();
+        Library library = new Library();
+        library.writeFile(name, pass);
+        System.out.println("Registration successful");
     }
 
 }
